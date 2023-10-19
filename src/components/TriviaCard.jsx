813dfb9a3b60fd60245bbda4c4f6f83481cce671
EddyJCuesta <cuesta79@gmail.com>
2023-10-18 21:11:01 -0400

    added function to the app, made it so the user can select an answer, if correct a point is added if incorrect a point is deducted

--- src/components/TriviaCard.jsx
+++ src/components/TriviaCard.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function TriviaCard({ question }) {
+function TriviaCard({ question, onAnswerSelect }) {
   const { question: questionText, correct_answer, incorrect_answers } = question;
 
   const allAnswers = [...incorrect_answers, correct_answer];
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+  const handleOptionClick = (option) => {
+    setSelectedAnswer(option);
+    onAnswerSelect(option === correct_answer);
+  };
 
   return (
     <div>
       <h3>{questionText}</h3>
       <ul>
         {allAnswers.map((option, index) => (
-          <li key={index}>{option}</li>
+          <li
+            key={index}
+            onClick={() => handleOptionClick(option)}
+            className={selectedAnswer === option ? 'selected' : ''}
+          >
+            {option}
+          </li>
         ))}
       </ul>
     </div>
--- src/components/TriviaList.jsx
+++ src/components/TriviaList.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TriviaCard from './TriviaCard';
 
 function TriviaList({ questions }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerSelect = (isCorrect) => {
+    if (isCorrect) {
+      setScore((prevScore) => prevScore + 1);
+    }
+
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
   return (
     <div>
-      {questions.map((question, index) => (
-        <TriviaCard key={index} question={question} />
-      ))}
+      {currentIndex < questions.length ? (
+        <TriviaCard
+          question={questions[currentIndex]}
+          onAnswerSelect={handleAnswerSelect}
+        />
+      ) : (
+        <div>
+          <h2>Trivia Completed!</h2>
+          <p>Your Score: {score}</p>
+        </div>
+      )}
     </div>
   );
 }
