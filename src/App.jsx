16857165e5fd516e2fe71d3b8bba9c36ea049ed1
EddyJCuesta <cuesta79@gmail.com>
2023-11-06 21:00:30 -0500

    created a landing page that contains rules and dificulty selection

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,44 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import TriviaList from './components/TriviaList';
+import LandingPage from './components/LandingPage';
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [selectedDifficulty, setSelectedDifficulty] = useState('easy');
 
   useEffect(() => {
-    axios({
-      url: "https://opentdb.com/api.php?amount=10&category=29",
-      method: "GET",
-      responseType: "json",
-      params: {},
-    })
-      .then((res) => {
-        setQuestions(res.data.results); // Update the state with fetched questions
-        console.log(res.data.results)
+    if (gameStarted) {
+      axios({
+        url: `https://opentdb.com/api.php?amount=10&category=29&difficulty=${selectedDifficulty}`,
+        method: 'GET',
+        responseType: 'json',
+        params: {},
       })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+        .then((res) => {
+          setQuestions(res.data.results); // Update the state with fetched questions
+          console.log(res.data.results);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }
+  }, [gameStarted, selectedDifficulty]);
+
+  const handleStartGame = (difficulty) => {
+    setGameStarted(true);
+    setSelectedDifficulty(difficulty);
+  };
 
   return (
     <div className="App">
       <h1>Trivia</h1>
-      <TriviaList questions={questions} />
+      {gameStarted ? (
+        <TriviaList questions={questions} />
+      ) : (
+        <LandingPage onStartGame={handleStartGame} />
+      )}
     </div>
   );
 }
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+
+function LandingPage({ onStartGame }) {
+  const [difficulty, setDifficulty] = useState('easy');
+
+  const handleDifficultyChange = (event) => {
+    setDifficulty(event.target.value);
+  };
+
+  const startGame = () => {
+    // Pass the selected difficulty to the parent component (App)
+    onStartGame(difficulty);
+  };
+
+  return (
+    <div className="landing-page">
+      <h1>Welcome to Trivia!</h1>
+      <p>Test your knowledge with a fun trivia game. Select the difficulty level and start answering questions.</p>
+      <form>
+        <label>
+          Select Difficulty:
+          <select value={difficulty} onChange={handleDifficultyChange}>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </label>
+      </form>
+      <button onClick={startGame}>Start Game</button>
+    </div>
+  );
+}
+
+export default LandingPage;
